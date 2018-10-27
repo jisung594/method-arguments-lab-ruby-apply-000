057b14ec5c4e9281dee8fd3fb4ff25c332cd2a8d
jonathan choi <jisung594@gmail.com>
2018-10-27 18:09:43 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,3 +5,11 @@ end
 def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+
+#{name}
+#{name}
+#{language}
+#{language}
+#{language}
+#{name}
+#{}
